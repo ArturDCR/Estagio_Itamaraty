de4b3429872c9565a3f26e11618f68c6d5c8b34c
ArturDCR <arturdecamargos@gmail.com>
2025-01-20 10:32:12 -0300

    Atualização analisador de faltas

--- utils/analise_de_faltas/Analise_de_faltas.py
+++ utils/analise_de_faltas/Analise_de_faltas.py
@@ -7,47 +7,54 @@ class Analise_de_faltas:
     def __init__(self):
         self.__FALTAS = pd.read_excel(os.path.join("utils/analise_de_faltas/dados","Faltas.xlsx"))
         self.__SAIDA = open('utils/analise_de_faltas/dados/saida.txt', 'w')
-        self.__SCE = pd.read_excel(os.path.join('utils/analise_de_faltas/dados/Sce.xlsx'))
 
     def __swicth (self, aux):
-        if len(str(aux)) == 2:
-            return f'000000{aux},00'
-        elif len(str(aux)) == 3:
-            return f'00000{aux},00'
-        elif len(str(aux)) == 4:
-            return f'0000{aux},00'
-        elif len(str(aux)) == 5:
-            return f'000{aux},00'
-        elif len(str(aux)) == 6:
-            return f'00{aux},00'
-        elif len(str(aux)) == 7:
-            return f'0{aux},00'
-        elif len(str(aux)) == 8:
-            return f'{aux},00'
-
-    def __conversor_de_cpf(self, cpf):
-            if len(cpf) != 11 and cpf[0] != '0' and '.' not in cpf:
-                cpf = '0' + cpf
-                if len(cpf) != 11:
-                    pass
-                else:
-                    return str(cpf)
-            elif len(cpf) != 11:
-                    return str(cpf[:3] + cpf[4:7] + cpf[8:11] + cpf[12:])
-            else:
-                return str(cpf)
+        if str(aux).__contains__('.'):
+            if len(str(aux).split('.')[-1]) == 2:
+                return f'000000{str(aux).replace('.',',')}'
+            elif len(str(aux).split('.')[-1]) == 3:
+                return f'00000{str(aux).replace('.',',')}'
+            elif len(str(aux).split('.')[-1]) == 4:
+                return f'0000{str(aux).replace('.',',')}'
+            elif len(str(aux).split('.')[-1]) == 5:
+                return f'000{str(aux).replace('.',',')}'
+            elif len(str(aux).split('.')[-1]) == 6:
+                return f'00{str(aux).replace('.',',')}'
+            elif len(str(aux).split('.')[-1]) == 7:
+                return f'0{str(aux).replace('.',',')}'
+            elif len(str(aux).split('.')[-1]) == 8:
+                return f'{str(aux).replace('.',',')}'
+        else:
+            if len(str(aux)) == 2:
+                return f'000000{aux},00'
+            elif len(str(aux)) == 3:
+                return f'00000{aux},00'
+            elif len(str(aux)) == 4:
+                return f'0000{aux},00'
+            elif len(str(aux)) == 5:
+                return f'000{aux},00'
+            elif len(str(aux)) == 6:
+                return f'00{aux},00'
+            elif len(str(aux)) == 7:
+                return f'0{aux},00'
+            elif len(str(aux)) == 8:
+                return f'{aux},00'
 
     def __gerar_dados(self, escolha, mes, ano):
         if escolha == 'VT':
             for a in range(len(self.__FALTAS.iloc[:,0])):
-                if str(self.__FALTAS.iloc[a,2]) != 'nan':
-                    self.__SAIDA.write(f'{(self.__FALTAS.iloc[a,3])},D,82695,6,I,{str(mes)+str(ano)},"{self.__swicth(int(self.__FALTAS.iloc[a,2])*10)}","SCE,Folha de ponto e ficha F","{self.__FALTAS.iloc[a,4]}"\n')
+                if str(self.__FALTAS.iloc[a,1]) != 'NÃO ENCONTRADO':
+                    if str(self.__FALTAS.iloc[a,2]) != 'nan':
+                        self.__SAIDA.write(f'{(self.__FALTAS.iloc[a,3])},D,82695,6,I,{str(mes)+str(ano)},"{self.__swicth(self.__FALTAS.iloc[a,5])}","SCE,Folha de ponto e ficha F","{self.__FALTAS.iloc[a,4]}"\n')
+                else:
+                    pass       
         else:
             for c in range(len(self.__FALTAS.iloc[:,0])):
-                if str(self.__FALTAS.iloc[c,2]) != 'nan':
-                    for d in range(len(self.__SCE.iloc[:,6])):
-                        if self.__conversor_de_cpf(str(self.__SCE.iloc[d,6])) == self.__conversor_de_cpf(str(self.__FALTAS.iloc[c,1])):
-                            self.__SAIDA.write(f'{(self.__FALTAS.iloc[c,3])},D,83172,6,I,{str(mes)+str(ano)},"{self.__swicth(int(self.__FALTAS.iloc[c,2])*float(str(self.__SCE.iloc[d,22]).replace(',','.')))}","SCE,Folha de ponto e ficha F","{self.__FALTAS.iloc[c,4]}"\n')
+                if str(self.__FALTAS.iloc[c,1]) != 'NÃO ENCONTRADO':
+                    if str(self.__FALTAS.iloc[c,2]) != 'nan':
+                        self.__SAIDA.write(f'{(self.__FALTAS.iloc[c,3])},D,83172,6,I,{str(mes)+str(ano)},"{self.__swicth(self.__FALTAS.iloc[c,5])}","SCE,Folha de ponto e ficha F","{self.__FALTAS.iloc[c,4]}"\n')
+                else:
+                    pass
 
     def __gerar_saida(self):
         self.__SAIDA.close()
--- utils/analise_de_faltas/Gerador_de_faltas.py
+++ utils/analise_de_faltas/Gerador_de_faltas.py
@@ -15,7 +15,8 @@ class Gerador_de_faltas:
             'cpf': [],
             'dias_justificados': [],
             'siape': [],
-            'dias': []
+            'dias': [],
+            'valor_total': []
         }
         
         self.__dados_BE = {
@@ -23,7 +24,9 @@ class Gerador_de_faltas:
             'cpf': [],
             'dias_injustificados': [],
             'siape': [],
-            'dias': []
+            'dias': [],
+            'valor_total': [],
+            'salario' : []
         }
 
     def __conversor_de_cpf(self, cpf):
@@ -38,74 +41,89 @@ class Gerador_de_faltas:
             else:
                 return str(cpf)
             
-    def __gerar_dados(self,escolha, mes, ano):
+    def __gerar_dados(self, escolha, mes, ano):
         for p in range(len(self.__FORMS.iloc[:,17])):
             if str(self.__FORMS.iloc[p,8]).__contains__(mes) and str(self.__FORMS.iloc[p,8]).__contains__(ano):
                 if escolha == 'VT':
                     if str(self.__FORMS.iloc[p,23]) != 'nan':
+                        #Bloco de tratamento dos dados de acordo com nome e cpf encontrados no forms ou não.
                         if str(self.__FORMS.iloc[p,11]) == 'NÃO ENCONTRADO':
-                            self.__dados_VT['nome'].append('NÃO ENCONTRADO')
+                            self.__dados_VT['nome'].append(str(self.__FORMS.iloc[p,14]))
                             self.__dados_VT['cpf'].append('NÃO ENCONTRADO')
                         else:
                             self.__dados_VT['nome'].append(str(self.__FORMS.iloc[p,11]).split('|')[0])
                             self.__dados_VT['cpf'].append(self.__conversor_de_cpf(str(self.__FORMS.iloc[p,11]).split(' | ')[-1]))
+                        #Bloco de tratamento com a quantidade de dias faltosos.
                         if str(self.__FORMS.iloc[p,23]).__contains__(';'):
                             self.__dados_VT['dias_justificados'].append(len(self.__FORMS.iloc[p,23].split(';')))
                             self.__dados_VT['dias'].append(f"{self.__FORMS.iloc[p,23].split(';')} de {mes}")
+                            #Linha que adiciona o total a ser descontado de acordo com a quantidade de faltas.
+                            self.__dados_VT['valor_total'].append(len(self.__FORMS.iloc[p,23].split(';'))*10)
                         else:
-                            self.__dados_VT['dias_justificados'].append(self.__FORMS.iloc[p,23])
+                            self.__dados_VT['dias_justificados'].append(len(self.__FORMS.iloc[p,23].split()))
                             self.__dados_VT['dias'].append(f'{self.__FORMS.iloc[p,23]} de {mes}')
+                            #Linha que adiciona o total a ser descontado de acordo com a quantidade de faltas.
+                            self.__dados_VT['valor_total'].append(len(self.__FORMS.iloc[p,23].split())*10)
                 else:
-                    if str(self.__FORMS.iloc[p,32]) != 'nan':               
+                    if str(self.__FORMS.iloc[p,32]) != 'nan':
+                        #Bloco de tratamento dos dados de acordo com nome e cpf encontrados no forms ou não.
                         if str(self.__FORMS.iloc[p,11]) == 'NÃO ENCONTRADO':
-                            self.__dados_BE['nome'].append('NÃO ENCONTRADO')
+                            self.__dados_BE['nome'].append(str(self.__FORMS.iloc[p,14]))
                             self.__dados_BE['cpf'].append('NÃO ENCONTRADO')
+                            self.__dados_BE['valor_total'].append('Sem informações')
                         else:
                             self.__dados_BE['cpf'].append(self.__conversor_de_cpf(str(self.__FORMS.iloc[p,11]).split(' | ')[-1]))
                             self.__dados_BE['nome'].append(str(self.__FORMS.iloc[p,11]).split('|')[0])
+                            #Laço dedicado a fazer o cálculo do desconto de acordo com o salário no relatório SCE, arredondando duas casas.
+                            for x in range(len(self.__SCE.iloc[:,6])):
+                                if self.__conversor_de_cpf(str(self.__SCE.iloc[x,6])) == self.__conversor_de_cpf(str(self.__FORMS.iloc[p,11]).split(' | ')[-1]):
+                                    self.__dados_BE['valor_total'].append(round(len(self.__FORMS.iloc[p,32].split(';'))*(float(str(self.__SCE.iloc[x,22]).replace(',','.'))/30),2))
+                        #Bloco de tratamento com a quantidade de dias faltosos.
                         if str(self.__FORMS.iloc[p,32]).__contains__(';'):
                             self.__dados_BE['dias_injustificados'].append(len(self.__FORMS.iloc[p,32].split(';')))
                             self.__dados_BE['dias'].append(f'{self.__FORMS.iloc[p,32].split(';')} de {mes}')
                         else:
-                            self.__dados_BE['dias_injustificados'].append(self.__FORMS.iloc[p,32])
+                            self.__dados_BE['dias_injustificados'].append(len(self.__FORMS.iloc[p,32].split()))
                             self.__dados_BE['dias'].append(f'{self.__FORMS.iloc[p,32]} de {mes}')
+                        #Laço dedicaco a encontrar o salário do estagiário no relatório SCE.
+                        for z in range(len(self.__SCE.iloc[:,6])):
+                            if self.__conversor_de_cpf(str(self.__SCE.iloc[z,6])) == self.__conversor_de_cpf(str(self.__FORMS.iloc[p,11]).split(' | ')[-1]):
+                                self.__dados_BE['salario'].append(self.__SCE.iloc[z,22])
 
         for c in self.__dados_VT['cpf']:
-                for b in range(len(self.__MRE.iloc[:,3])):
-                    if c == self.__conversor_de_cpf(str(self.__MRE.iloc[b,3])):
-                        self.__dados_VT['siape'].append(self.__MRE.iloc[b,1])
-                    elif c == 'NÃO ENCONTRADO':
-                        self.__dados_VT['siape'].append('xxx')
-                        
+            for b in range(len(self.__MRE.iloc[:,3])):
+                if c == self.__conversor_de_cpf(str(self.__MRE.iloc[b,3])):
+                    self.__dados_VT['siape'].append(self.__MRE.iloc[b,1])
+                    break
+                elif c == 'NÃO ENCONTRADO':
+                    self.__dados_VT['siape'].append('xxx')
+                    break
+           
         for c in self.__dados_BE['cpf']:
-                for b in range(len(self.__MRE.iloc[:,3])):
-                    if c == self.__conversor_de_cpf(str(self.__MRE.iloc[b,3])):
-                        self.__dados_BE['siape'].append(self.__MRE.iloc[b,1])
-                    elif c == 'NÃO ENCONTRADO':
-                        self.__dados_BE['siape'].append('xxx')
-
-        for e in self.__dados_VT['cpf']:
-            for d in range(len(self.__SCE.iloc[:,6])):
-                if self.__conversor_de_cpf(str(self.__SCE.iloc[d,6])) == e:
-                    if str(self.__SCE.iloc[d,28]) != 'nan':
-                        index = self.__dados_VT['cpf'].index(e)
-                        self.__dados_VT['nome'].pop(index)
-                        self.__dados_VT['cpf'].pop(index)
-                        self.__dados_VT['siape'].pop(index)
-                        self.__dados_VT['dias_justificados'].pop(index)
-                        self.__dados_VT['dias_injustificados'].pop(index)
+            for b in range(len(self.__MRE.iloc[:,3])):
+                if c == self.__conversor_de_cpf(str(self.__MRE.iloc[b,3])):
+                    self.__dados_BE['siape'].append(self.__MRE.iloc[b,1])
+                elif c == 'NÃO ENCONTRADO':
+                    self.__dados_BE['siape'].append('xxx')
+                    break
+        for z in range(5):
+            for e in self.__dados_VT['cpf']:
+                for d in range(len(self.__SCE.iloc[:,6])):
+                    if self.__conversor_de_cpf(str(self.__SCE.iloc[d,6])) == e:
+                        if str(self.__SCE.iloc[d,13]) == 'Inativo':
+                            index = self.__dados_VT['cpf'].index(e)
+                            for g in self.__dados_VT:
+                                self.__dados_VT[g].remove(self.__dados_VT[g][index])
         
-        for e in self.__dados_BE['cpf']:
-            for d in range(len(self.__SCE.iloc[:,6])):
-                if self.__conversor_de_cpf(str(self.__SCE.iloc[d,6])) == e:
-                    if str(self.__SCE.iloc[d,28]) != 'nan':
-                        index = self.__dados_BE['cpf'].index(e)
-                        self.__dados_BE['nome'].pop(index)
-                        self.__dados_BE['cpf'].pop(index)
-                        self.__dados_BE['siape'].pop(index)
-                        self.__dados_BE['dias_justificados'].pop(index)
-                        self.__dados_BE['dias_injustificados'].pop(index)
-    
+        for z in range(5):
+            for e in self.__dados_BE['cpf']:
+                for d in range(len(self.__SCE.iloc[:,6])):
+                    if self.__conversor_de_cpf(str(self.__SCE.iloc[d,6])) == e:
+                        if str(self.__SCE.iloc[d,28]) != 'nan':
+                            index = self.__dados_BE['cpf'].index(e)
+                            for g in self.__dados_BE:
+                                self.__dados_BE[g].remove(self.__dados_BE[g][index])
+
     def __gerar_saida(self, escolha):
         if escolha == 'VT':
             pd.DataFrame(self.__dados_VT).to_excel('utils/analise_de_faltas/dados/Faltas.xlsx',index=False)
@@ -118,7 +136,7 @@ class Gerador_de_faltas:
         for chave in self.__dados_BE:
             self.__dados_BE[chave].clear()
         for key in self.__dados_VT:
-            self.__dados_VT[chave].clear()
+            self.__dados_VT[key].clear()
     
     def iniciar(self, escolha, mes, ano):
         self.__gerar_dados(escolha, mes, ano)
